b60af1e78ae74ea320e935231f97e5bcdc6bc78a
ericKim <erickim000@gmail.com>
2019-11-16 14:52:30 +0900

    4th

--- 1.c
+++ 1.c
@@ -1,9 +1,11 @@
-Reinitialized existing Git repository in /mnt/c/Users/erick/ericttttt/.git/
+ssReinitialized existing Git repository in /mnt/c/Users/erick/ericttttt/.git/
+ssReinitialized existing Git repository in /mnt/c/Users/erick/ericttttt/.git/
 [master (root-commit) b4c2b20] first commit
  1 file changed, 1 insertion(+)
+ssReinitialized existing Git repository in /mnt/c/Users/erick/ericttttt/.git/
  create ddddmode 100644 README.md
 Writing ddddd123455objecffts: 100% (3/3), 217 bytes | 108.00 KiB/s, done.
-Total 3 (delta 0), reused 0 (delta 0)
+ssTotal 3 (delta 0), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
