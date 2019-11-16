2ff5c3aafd59e54be4715ef322472032dd9d7f50
ericKim <erickim000@gmail.com>
2019-11-16 14:34:16 +0900

    1st

---
+++ 1.c
@@ -0,0 +1,11 @@
+Reinitialized existing Git repository in /mnt/c/Users/erick/ericttttt/.git/
+[master (root-commit) b4c2b20] first commit
+ 1 file changed, 1 insertion(+)
+ create mode 100644 README.md
+fatal: remote origin already exists.
+Username for 'https://github.com': ericnjin
+Password for 'https://ericnjin@github.com':
+Counting objects: 3, done.
+Writing objects: 100% (3/3), 217 bytes | 108.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0)
+To https://github.com/ericnjin/ericttttt.git
