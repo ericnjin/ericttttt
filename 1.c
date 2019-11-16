f7bc89a03def4b04c2ff3363dae93601c3e89e4a
ericKim <erickim000@gmail.com>
2019-11-16 14:43:47 +0900

    HB 2nd

--- 1.c
+++ 1.c
@@ -2,13 +2,13 @@ Reinitialized existing Git repository in /mnt/c/Users/erick/ericttttt/.git/
 [master (root-commit) b4c2b20] first commit
  1 file changed, 1 insertion(+)
  create ddddmode 100644 README.md
-Writing objecffts: 100% (3/3), 217 bytes | 108.00 KiB/s, done.
+Writing 123455objecffts: 100% (3/3), 217 bytes | 108.00 KiB/s, done.
 Total 3 (delta 0), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
-Total 3 (delta 0), reused 0 (delta 0)
+Total 3 (delta 11110), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
 Total 3 (delta 0), reused 0 (delta 0)
